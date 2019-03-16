babb14e1533a8d0747fb39fb2d433ae9e5d6225e
JavaClass2019 <C15717626@mydit.ie>
2019-03-16 17:10:09 +0200

    chore(Repository.java): Add Repository Javadoc

--- Repository.java
+++ Repository.java
@@ -1,6 +1,15 @@
 
 import java.util.List;
 
+/**
+ * A Repository for retrieving, creating, updating and deleting Employee
+ * records. Provides functionality to get first, last, previous and next
+ * records and as such relies on a cursor-based database. Which basically means
+ * that the implementing class MUST be able to keep track of the current position
+ * we are in the database.
+ * 
+ */
+
 public interface Repository {
 
     /**
@@ -28,6 +37,12 @@ public interface Repository {
      * @return
      */
     List<Employee> findAll();
+
+    /**
+     * Gets the previous record from the current cursor on the database
+     * @return
+     */
+    Employee previousRecord();
 
     /**
      * Gets the next record from the current cursor on the database
